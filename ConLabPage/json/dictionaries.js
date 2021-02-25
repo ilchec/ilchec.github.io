3e7043537694c8c8c647db4cbdc859a940ac0ae1
ilchec <>
2021-02-25 17:06:00 +0100

    sfds

--- ConLabPage/json/dictionaries.js
+++ ConLabPage/json/dictionaries.js
@@ -1,18 +1,18 @@
 var dictionariesData = '['+
  '{ "title": "Mehweb I",' +
     '"url": "https://linghub.ru/mehwebdict/",' +
-    '"image": "/images/1.JPG",' +
+    '"image": "ConLabPage/images/1.JPG",' +
     '"exerpt": "Lorem Ipsum blabla interesting preview to the site"},' +
   '{ "title": "Mehweb II",' +
        '"url": "http://lingconlab.ru/MehwebDict/",' +
-       '"image": "/images/2.JPG",' +
+       '"image": "ConLabPage/images/2.JPG",' +
        '"exerpt": "Lorem Ipsum blabla interesting preview to the site"},' +
   '{ "title": "Rutul",' +
      '"url": "https://linghub.ru/rutuldict/",' +
-     '"image": "/images/1.JPG",' +
+     '"image": "ConLabPage/images/1.JPG",' +
      '"exerpt": "Lorem Ipsum blabla interesting preview to the site"},' +
   '{ "title": "Tukita",' +
      '"url": "http://lingconlab.ru/TukitaDict/",' +
-     '"image": "/images/2.JPG",' +
+     '"image": "ConLabPage/images/2.JPG",' +
      '"exerpt": "Lorem Ipsum blabla interesting preview to the site"}' +
 ']';
