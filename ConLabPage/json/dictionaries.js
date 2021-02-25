70ce787fc8461dedd097a6be3be2a077ea8b7c5d
ilchec <>
2021-02-25 15:51:56 +0100

    dsf

--- ConLabPage/json/dictionaries.js
+++ ConLabPage/json/dictionaries.js
@@ -1,8 +1,1 @@
-var dictionariesData = '[
-  {"name" : "Harry", "age" : "32"},
-  {"name":"Hannah", "age": "45"},
-  {"name":"Peter", "age": "70"},
-  {"name":"Joe", "age": "19"}, 
-  {"name":"Klara", "age": "80"},
-  {"name":"Robert", "age": "17"},
-  {"name":"Karl", "age": "65"}]';
+var dictionariesData = '[{"name" : "Harry", "age" : "32"}, {"name":"Hannah", "age": "45"}, {"name":"Peter", "age": "70"}, {"name":"Joe", "age": "19"},  {"name":"Klara", "age": "80"}, {"name":"Robert", "age": "17"}, {"name":"Karl", "age": "65"}]';
