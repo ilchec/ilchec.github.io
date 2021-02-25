d280af788c1fb53f100fac6c3fe6aa17a3a13f5e
ilchec <>
2021-02-25 16:46:46 +0100

    dfsd

--- ConLabPage/json/dictionaries.js
+++ ConLabPage/json/dictionaries.js
@@ -1,1 +1,18 @@
-var dictionariesData = '[{"name" : "Harry", "age" : "32"}, {"name":"Hannah", "age": "45"}, {"name":"Hannah", "age": "45"}, {"name":"Hannah", "age": "45"}, {"name":"Hannah", "age": "45"}, {"name":"Hannah", "age": "45"}]';
+var dictionariesData = '['+
+ '{ "title": "Mehweb I",' +
+    '"url": "https://linghub.ru/mehwebdict/",' +
+    '"image": "/images/blog/blog-article-1.png",' +
+    '"exerpt": "Lorem Ipsum blabla interesting preview to the site"},' +
+  '{ "title": "Mehweb II",' +
+       '"url": "http://lingconlab.ru/MehwebDict/",' +
+       '"image": "/images/blog/blog-article-1.png",' +
+       '"exerpt": "Lorem Ipsum blabla interesting preview to the site"},' +
+  '{ "title": "Rutul",' +
+     '"url": "https://linghub.ru/rutuldict/",' +
+     '"image": "/images/blog/blog-article-1.png",' +
+     '"exerpt": "Lorem Ipsum blabla interesting preview to the site"},' +
+  '{ "title": "Tukita",' +
+     '"url": "http://lingconlab.ru/TukitaDict/",' +
+     '"image": "/images/blog/blog-article-1.png",' +
+     '"exerpt": "Lorem Ipsum blabla interesting preview to the site"},' +
+']';
