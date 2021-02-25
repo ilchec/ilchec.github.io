2af6f3ea03c7bd1523b979a4cc2d16c54cf4fe4f
ilchec <>
2021-02-25 15:14:59 +0100

    fsdf

--- ConLabPage/json/dictionaries.js
+++ ConLabPage/json/dictionaries.js
@@ -1,17 +1,9 @@
-dictionariesContent = '[
-    { "title": "blog article 1",
-      "url": "/blog/blog-article-1.html",
-      "image": "/images/blog/blog-article-1.png",
-      "exerpt": "Lorem Ipsum blabla interesting preview to the site",
-      "tags": ["a", "b", "c"]},
-    { "title": "blog article 2",
-      "url": "/blog/blog-article-2.html",
-      "image": "/images/blog/blog-article-2.png",
-      "exerpt": "Lorem Ipsum blabla interesting preview to the site",
-      "tags": ["a", "c"]},
-    { "title": "blog article 3",
-      "url": "/blog/blog-article-3.html",
-      "image": "/images/blog/blog-article-3.png",
-      "exerpt": "Lorem Ipsum blabla interesting preview to the site",
-      "tags": ["a"]}
-]'
+var dictionariesContent = '
+{
+"articles": [
+{"title": "blog article 1", "url": "/blog/blog-article-1.html", "image": "/images/blog/blog-article-1.png", "exerpt": "Lorem Ipsum blabla interesting preview to the site", "tags": ["a", "b", "c"]},
+{"title": "blog article 2", "url": "/blog/blog-article-2.html", "image": "/images/blog/blog-article-2.png", "exerpt": "Lorem Ipsum blabla interesting preview to the site", "tags": ["a", "c"]},
+{"title": "blog article 3", "url": "/blog/blog-article-3.html", "image": "/images/blog/blog-article-3.png", "exerpt": "Lorem Ipsum blabla interesting preview to the site", "tags": ["a"]},
+]
+}
+';
